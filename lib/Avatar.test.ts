a07c097df358edd5fe2f4abc8a9cc43048346f52
Yue Yang <g1enyy0ung@gmail.com>
2022-11-29 21:25:03 +0800

    test: cover more props

--- lib/Avatar.test.ts
+++ lib/Avatar.test.ts
@@ -41,4 +41,112 @@ describe('Avatar', () => {
     expect(container).toMatchSnapshot()
     expect(getByText('JD')).toBeInTheDocument()
   })
+
+  it('if only the name is provided, it should render a letter avatar', () => {
+    vi.mocked(useLoaded).mockImplementationOnce(() => {
+      return {
+        loading: ref(false),
+        loaded: ref(false),
+      }
+    })
+    const { container, getByText } = render(Avatar, {
+      props: {
+        name: 'John Doe',
+      },
+    })
+
+    expect(container).toMatchSnapshot()
+    expect(getByText('JD')).toBeInTheDocument()
+  })
+
+  it('if bgColor is provided, it should render a letter avatar with the provided background color', () => {
+    vi.mocked(useLoaded).mockImplementationOnce(() => {
+      return {
+        loading: ref(false),
+        loaded: ref(false),
+      }
+    })
+    const { container, getByText } = render(Avatar, {
+      props: {
+        name: 'John Doe',
+        bgColor: 'red',
+      },
+    })
+
+    expect(container).toMatchSnapshot()
+    expect(getByText('JD')).toHaveStyle('background-color: red')
+  })
+
+  it('variant: rounded', () => {
+    vi.mocked(useLoaded).mockImplementationOnce(() => {
+      return {
+        loading: ref(false),
+        loaded: ref(true),
+      }
+    })
+    const { container } = render(Avatar, {
+      props: {
+        src: 'https://fakeimg.pl/300/',
+        variant: 'rounded',
+      },
+    })
+
+    expect(container).toMatchSnapshot()
+    expect(container.firstChild).toHaveStyle('border-radius: 8px')
+  })
+
+  it('variant: rounded + round 4px', () => {
+    vi.mocked(useLoaded).mockImplementationOnce(() => {
+      return {
+        loading: ref(false),
+        loaded: ref(true),
+      }
+    })
+    const { container } = render(Avatar, {
+      props: {
+        src: 'https://fakeimg.pl/300/',
+        variant: 'rounded',
+        round: 4,
+      },
+    })
+
+    expect(container).toMatchSnapshot()
+    expect(container.firstChild).toHaveStyle('border-radius: 4px')
+  })
+
+  it('variant: square', () => {
+    vi.mocked(useLoaded).mockImplementationOnce(() => {
+      return {
+        loading: ref(false),
+        loaded: ref(true),
+      }
+    })
+    const { container } = render(Avatar, {
+      props: {
+        src: 'https://fakeimg.pl/300/',
+        variant: 'square',
+      },
+    })
+
+    expect(container).toMatchSnapshot()
+    expect(container.firstChild).not.toHaveStyle('border-radius: 8px')
+  })
+
+  it('if fallback is false, it should not render a letter avatar when the image is not loaded', () => {
+    vi.mocked(useLoaded).mockImplementationOnce(() => {
+      return {
+        loading: ref(false),
+        loaded: ref(false),
+      }
+    })
+    const { queryByText } = render(Avatar, {
+      props: {
+        src: 'https://fakeimg.pl/300/',
+        name: 'John Doe',
+        fallback: false,
+      },
+    })
+
+    expect(queryByText('JD')).not.toBeInTheDocument()
+  })
 })
